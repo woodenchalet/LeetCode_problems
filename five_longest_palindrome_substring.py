072ccc15e8bbe1e4aaee30261efbe8b6e728db5c
wooden <chalet056@gmail.com>
2016-08-28 19:14:27 -0400

    longest palindrome

---
+++ five_longest_palindrome_substring.py
@@ -0,0 +1,27 @@
+str = "dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd"
+longest = ''
+index = 0
+
+for char in str:
+    count = 0
+    temp = ''
+    if str[index - count] == str[index + count]:
+        while index - count >= 0 and index + count < len(str):
+            if count == 0:
+                temp += char
+            else:
+                temp = str[index - count] + temp + str[index + count]
+        count += 1
+    elif str[index - count] == str[index + count + 1]:
+        while index + count + 1 < len(str) and index - count >= 0:
+            temp = str[index - count] + temp + str[index + count + 1]
+            count += 1
+    else:
+        continue
+
+    if len(temp) > len(longest):
+        longest = temp
+
+    index += 1
+
+print longest
