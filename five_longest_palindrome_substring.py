7a17a2e2fba1b636f3a6afec39b7a267a45b3a63
wooden <chalet056@gmail.com>
2016-09-03 22:36:25 -0400

    update five longest palindrome

--- five_longest_palindrome_substring.py
+++ five_longest_palindrome_substring.py
@@ -1,27 +1,43 @@
-str = "dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd"
+# str = "ddddddddddddddddddddddddddddddd" \
+#       "ddddddddddddddddddddddddddddddd" \
+#       "ddddddddddddddddddddddddddddddd" \
+#       "ddddddddddddddddddddddddddddddd" \
+#       "ddddddddddddddddddddddddddddddd" \
+#       "ddddddddddddddddddddddddddddddd" \
+#       "ddddddddddddddddddddddddddddddd" \
+#       "ddddddddddddddddddddddddddddddd" \
+#       "ddddddddddddddddddddddddddddddd" \
+#       "ddddddddddddddddddddddddddddddd" \
+#       "ddddddddddddddddddddddddddddddd" \
+#       "ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd"
+
+str = 'abba'
 longest = ''
 index = 0
 
-for char in str:
-    count = 0
-    temp = ''
-    if str[index - count] == str[index + count]:
-        while index - count >= 0 and index + count < len(str):
-            if count == 0:
-                temp += char
-            else:
-                temp = str[index - count] + temp + str[index + count]
-        count += 1
-    elif str[index - count] == str[index + count + 1]:
-        while index + count + 1 < len(str) and index - count >= 0:
-            temp = str[index - count] + temp + str[index + count + 1]
-            count += 1
-    else:
-        continue
+if len(str) == 1:
+    print str
+else:
+    for char in str:
+        count = 0
+        temp = ''
+        if str[index - count] == str[index + count]:
+            while index - count >= 0 and index + count < len(str) and str[index - count] == str[index + count]:
+                if count == 0:
+                    temp += char
+                else:
+                    temp = str[index - count] + temp + str[index + count]
+                count += 1
+        elif str[index - count] == str[index + count + 1]:
+            while index + count + 1 < len(str) and index - count >= 0 and str[index - count] == str[index + count + 1]:
+                temp = str[index - count] + temp + str[index + count + 1]
+                count += 1
+        else:
+            continue
 
-    if len(temp) > len(longest):
-        longest = temp
+        if len(temp) > len(longest):
+            longest = temp
 
-    index += 1
+        index += 1
 
-print longest
+    print longest
